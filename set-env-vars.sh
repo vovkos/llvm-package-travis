b405231da4e529171837a1562962bcc2a328bf5e
Vladimir Gladkov (laptop-arch) <vovkos@gmail.com>
2019-05-16 23:13:40 +0800

    attempt at fixing the tablegen issue

--- set-env-vars.sh
+++ set-env-vars.sh
@@ -89,8 +89,11 @@ elif [ $LLVM_VERSION >= "3.5.0" ]; then
 	CLANG_CMAKE_FLAGS+=(-DLLVM_CONFIG=$LLVM_RELEASE_DIR/bin/llvm-config)
 else # 3.4.2
 	LLVM_CMAKE_FLAGS+=(-DHAVE_SANITIZER_MSAN_INTERFACE_H=0)
-	CLANG_CMAKE_FLAGS+=(-DCLANG_PATH_TO_LLVM_BUILD=$LLVM_RELEASE_DIR)
-	CLANG_CPU_COUNT=1 # target dependencies in clang-3.4.2 are not properly set
+
+	CLANG_CMAKE_FLAGS+=(
+		-DCLANG_PATH_TO_LLVM_BUILD=$LLVM_RELEASE_DIR
+		-DLLVM_MAIN_SRC_DIR=$LLVM_RELEASE_DIR
+		)
 fi
 
 # don't build Debug tools -- executables will be huge and not really
