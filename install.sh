e9333c8997f45b3ff2d1ad7dce87f2c26935a64a
Vladimir Gladkov (office-win10) <vovkos@gmail.com>
2019-05-23 18:42:59 +0800

    added patch to YAMLTraits.h on llvm-3.5.0 .. llvm-3.9.1
    without this patch, Debug Clang builds for projects using LLVM libraries stop with 'undefined reference'

--- install.sh
+++ install.sh
@@ -17,6 +17,14 @@ case "$BUILD_PROJECT" in
 		echo "set_target_properties(llvm-config PROPERTIES EXCLUDE_FROM_ALL FALSE)" >> llvm/tools/llvm-config/CMakeLists.txt
 		echo "install(TARGETS llvm-config RUNTIME DESTINATION bin)" >> llvm/tools/llvm-config/CMakeLists.txt
 	fi
+
+	# patch YAMLTraits.h in llvm-3.5.0 .. llvm-3.9.1 as to avoid
+	# 'undefined reference' with Debug builds on Clang-7
+
+	if [[ $TRAVIS_DIST == "xenial" && $CC == "clang" && \
+		  $LLVM_VERSION > "3.4.9" && $LLVM_VERSION < "4.0.0" ]]; then
+		perl yaml-patch.pl llvm/include/llvm/Support/YAMLTraits.h
+	fi
 	;;
 
 "clang")
